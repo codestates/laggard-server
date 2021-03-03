02a0d44ac16895ae30e74330514827f7f400cc57
aesop <ee.changsub@gmail.com>
2021-03-03 20:03:41 +0900

    Complete isAnswerCorrect

--- src/controller/test/isAnswerCorrect.ts
+++ src/controller/test/isAnswerCorrect.ts
@@ -1,11 +1,35 @@
 import express, { Request, Response, NextFunction } from 'express';
+import { is } from 'sequelize/types/lib/operators';
 import { Songs } from '../../model/songs';
+import { Genres } from '../../model/genres';
+import { Tests_and_genres } from '../../model/tests_and_genres';
+import { Periods } from '../../model/periods';
+import { Tests_and_periods } from '../../model/tests_and_periods';
 const { QueryTypes, Op } = require('sequelize');
 
+const processGenre = (str: string) => {
+  if (/OST./gi.test(str)) {
+    console.log('OST');
+    return 'OST';
+  } else if (/.댄스/gi.test(str)) {
+    console.log('댄스');
+    return 'dance';
+  } else if (/.발라드/gi.test(str)) {
+    console.log('발라드');
+    return 'ballad';
+  } else if (/.힙합/gi.test(str)) {
+    console.log('힙합');
+    return '힙합';
+  } else {
+    console.log('장르없음');
+    return '';
+  }
+};
+
 export const isAnswerCorrect = async (req: Request, res: Response) => {
-  console.log('isAnswerCorrect')
-  console.log(req.body)
-  
+  console.log('isAnswerCorrect');
+  console.log(req.body);
+
   const id: number = req.body.songs_id;
   const submittedAnswer: string = req.body.answer;
 
@@ -13,18 +37,52 @@ export const isAnswerCorrect = async (req: Request, res: Response) => {
     res.sendStatus(400);
   }
 
-  let songInfo = await Songs.findOne({ where: { id }, attributes: ['id', 'title', 'year', 'genre']})
-  console.log(songInfo)
-  let title = songInfo.dataValues.title; // 'Frank Ocean (feat. SOLE)'
-  title = title.match(/.+(?=\()/)[0].replace(/\s/gi, "") // 'FrankOcean'
-  let re = new RegExp(title, 'i')
-  let isCorrect = re.test(submittedAnswer);
-  if(isCorrect) {
-    //점수 올리기
-    //장르별 점수 기록
-  } else {
-    //장르별 문제 푼 점수?
+  // type _songData = {
+  //   id: number,
+  //   title: string,
+  //   year: number,
+  //   genre: string
+  // }
+  let songData = await Songs.findOne({
+    where: { id },
+    attributes: ['id', 'title', 'year', 'genre'],
+  });
+  console.log(songData);
+
+  if (songData) {
+    //* 장르 판별
+    let genre: string = songData.getDataValue('genre');
+    genre = processGenre(genre);
+    console.log(genre);
+
+    //* 정답 판별
+    let title: any = songData.getDataValue('title');
+    if (/\(/g.test(title)) {
+      title = title.match(/.+(?=\()/g)[0];
+    }
+    title = title.replace(/\s/gi, '');
+    console.log(title);
+    let re = new RegExp(title, 'i');
+    let isCorrect: boolean = re.test(submittedAnswer);
+    console.log(isCorrect);
+
+    //* 장르 점수 넣기
+    // isCorrect ?
+
+    res.status(201).send({ data: isCorrect, message: 'Created.' });
   }
 
-  res.status(200).send({ data: isCorrect, message: "Created."})
-}
+  // let title = songData.; // 'Frank Ocean (feat. SOLE)'
+  // console.log(title);
+  // title = title.match(/.+(?=\()/)[0].replace(/\s/gi, "") // 'FrankOcean'
+  // let re = new RegExp(title, 'i')
+  // let isCorrect = re.test(submittedAnswer);
+  // if (isCorrect) {
+  // 점수 올리기
+  //장르별 점수 기록
+  // } else {
+  //장르별 문제 푼 점수?
+  // }
+
+  // res.status(200).send({ data: isCorrect, message: 'Created.' });
+};
--- src/model/tests.ts
+++ src/model/tests.ts
@@ -1,53 +1,52 @@
 import { sequelize } from './index';
 import { Sequelize, Model, DataTypes } from 'sequelize';
 
-
 interface testsAttributes {
-    id : number | null;
-    types_id : number;
-    birth_year : number;
-    sex : boolean;
+  id: number | null;
+  types_id: number | null; // or null 부분 추가했습니다.
+  birth_year: number;
+  sex: boolean;
 }
 
-
 export class Tests extends Model<testsAttributes> {
-    public readonly id! : number;
-    public types_id! : number;
-    public birth_year! : number;
-    public sex! : boolean;
+  public readonly id!: number;
+  public types_id!: number;
+  public birth_year!: number;
+  public sex!: boolean;
 
-    public readonly createdAt! : Date;
-    public readonly updatedAt! : Date;
+  public readonly createdAt!: Date;
+  public readonly updatedAt!: Date;
 }
 
 Tests.init(
-    {
-        id : {
-            type : DataTypes.BIGINT.UNSIGNED,
-            autoIncrement : true,
-            primaryKey : true
-        },
-        types_id : {
-            type : DataTypes.BIGINT.UNSIGNED,
-            references : {
-                model: "Types",
-                key : "id"
-            },
-        },
-        birth_year : {
-            type : DataTypes.INTEGER,
-            allowNull : false
-        },
-        sex : {
-            type : DataTypes.BOOLEAN,
-            allowNull : false
-        }
-    },{
-        sequelize,
-        tableName : 'Tests',
-        modelName : 'Tests',
-        freezeTableName : true,
-        timestamps : true,
-        updatedAt : 'updateTimestamp'
-    }
-)
+  {
+    id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      autoIncrement: true,
+      primaryKey: true,
+    },
+    types_id: {
+      type: DataTypes.BIGINT.UNSIGNED,
+      references: {
+        model: 'Types',
+        key: 'id',
+      },
+    },
+    birth_year: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+    sex: {
+      type: DataTypes.BOOLEAN,
+      allowNull: false,
+    },
+  },
+  {
+    sequelize,
+    tableName: 'Tests',
+    modelName: 'Tests',
+    freezeTableName: true,
+    timestamps: true,
+    updatedAt: 'updateTimestamp',
+  },
+);
