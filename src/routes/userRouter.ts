3f3beb63a1ac7274bf88d3bd64d4c3be95721c40
Jaehyeon Lim <xhwogusxh@gmail.com>
2021-03-06 22:00:20 +0900

    socialLoginModify

--- src/routes/userRouter.ts
+++ src/routes/userRouter.ts
@@ -21,6 +21,8 @@ userRouter.post('/signup/basic',controller.default.postUser.signup);
 userRouter.post('/signin/basic',controller.default.postUser.signin);
 userRouter.post('/signout', controller.default.postUser.signout);
 userRouter.post('/signup/emailCheck', controller.default.postUser.checkEmailDuplicate);
+userRouter.post('/signup/social', controller.default.postUser.socialSignup);
+userRouter.post('/signin/social', controller.default.postUser.socialSignin);
 
 //PUT
 userRouter.put('/userinfo', controller.default.putUser.modify);
