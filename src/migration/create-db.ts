54dc328fb1566ee987dd25c8ceb225dbfcda9249
Jaehyeon Lim <xhwogusxh@gmail.com>
2021-03-13 16:46:47 +0900

    cors Setting

--- src/migration/create-db.ts
+++ src/migration/create-db.ts
@@ -6,11 +6,17 @@ class options implements Options{
     dialect!: 'mysql';
     username!: string;
     password!: string;
+    host!: string;
+    port!: number;
+    database!: string;
 }        
 
 const createDBOptions = new options();
 createDBOptions.username = process.env.DB_USERNAME || 'root';
 createDBOptions.password = process.env.DB_PASSWORD || 'your password';
+createDBOptions.host = process.env.DB_HOST || 'localhost'
+createDBOptions.port = Number(process.env.DB_PORT) || 3306;
+
 createDBOptions.dialect = 'mysql';
 
 let db_name = process.env.DB_DBNAME || 'new DataBase';
